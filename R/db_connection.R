57721e6cd4912ee46d814a5546475b73a6100c83
Ke Min <856926+x2007tro@users.noreply.github.com>
2018-10-07 12:27:57 -0300

    Add files via upload
    db server connection

---
+++ R/db_connection.R
@@ -0,0 +1,48 @@
+##
+# Connect to mysql server
+##
+ConnMySql <- function(dbn){
+  srv <- "192.168.2.120"
+  prt <- 3774
+  id <- "dspeast2"
+  pwd <- "yuheng"
+  
+  conn <- DBI::dbConnect(drv = RMariaDB::MariaDB(),
+                         user = id,
+                         password = pwd,
+                         dbname = dbn,
+                         host = srv,
+                         port = prt)
+  return(conn)
+}
+
+##
+# Connect to sql server - windows authentication
+##
+ConnSqlServer_WinAuth <- function(dbn){
+  srv <- "192.168.2.120"
+  
+  conn <- DBI::dbConnect(drv = odbc::odbc(),
+                         Driver = "ODBC Driver 17 for SQL Server",
+                         Server = srv,
+                         Database = dbn,
+                         trusted_connection = "yes")
+  return(conn)
+}
+
+##
+# Connect to sql server - SQL authentication
+##
+ConnSqlServer_SQLAuth <- function(dbn){
+  srv <- "192.168.2.120"
+  id <- "dspeast2"
+  pwd <- "yuheng"
+  
+  conn <- DBI::dbConnect(drv = odbc::odbc(),
+                         Driver = "ODBC Driver 17 for SQL Server",
+                         Server = srv,
+                         Database = dbn,
+                         UID = id,
+                         PWD = pwd)
+  return(conn)
+}
